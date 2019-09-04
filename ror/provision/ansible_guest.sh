f6ed8f1e1796cb2af969bba03a4158ffea894786
crjcodes <crjohnson@codeonward.com>
2019-09-04 12:16:58 -0500

    Fully working vm with ruby and rails installed through apt and ansible

--- ror/provision/ansible_guest.sh
+++ ror/provision/ansible_guest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y ansible
-cp provision/inventories/dev /etc/ansible/hosts -f
+#cp provision/inventories/dev /etc/ansible/hosts -f
 #chmod 666 /etc/ansible/hosts
 #cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
-sudo ansible-playbook provision/provision.yml --connection=local
+#sudo ansible-playbook provision/provision.yml --connection=local
