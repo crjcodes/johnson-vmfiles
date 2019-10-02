8c0b39b199555bfa47d8c8ac841d3b4e5f8b3377
crjcodes <crjohnson@codeonward.com>
2019-10-02 14:45:11 -0500

    Attempt to colorize Vagrant VM output in Git Bash terminanl; see ansible.cfg.  Not working yet outside VSC terminal

--- ror/ansible/ansible_guest.sh
+++ ror/ansible/ansible_guest.sh
@@ -5,3 +5,34 @@ echo "............Updating apt..."
 sudo apt-get update
 echo "............Installing ansible..."
 sudo apt-get install -y ansible
+
+cat >> /home/vagrant/ansible.cfg <<EOL
+# config file for ansible -- https://ansible.com/
+# ===============================================
+
+# nearly all parameters can be overridden in ansible-playbook
+# or with command line flags. ansible will read ANSIBLE_CONFIG,
+# ansible.cfg in the current working directory, .ansible.cfg in
+# the home directory or /etc/ansible/ansible.cfg, whichever it
+# finds first
+
+[defaults]
+nocolor = 0
+
+[colors]
+highlight = white
+verbose = blue
+warn = bright purple
+error = red
+debug = dark gray
+deprecate = purple
+skip = cyan
+unreachable = red
+ok = green
+changed = yellow
+diff_add = green
+diff_remove = red
+diff_lines = cyan
+EOL
+
+
